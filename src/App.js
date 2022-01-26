1b621ed2165ce7d1331242be1dcc66cd34544eb9
Gomez0015 <oscargomez.raxo@gmail.com>
2022-01-26 15:10:06 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import Terminal from 'terminal-in-react';
 
 function App() {
+
+  const showMsg = () => 'Hello World'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh"
+        }}
+      >
+        <Terminal
+          color='green'
+          backgroundColor='black'
+          barColor='black'
+          startState='maximised'
+          style={{ fontWeight: "bold", fontSize: "1em" }}
+          hideTopBar='true'
+          commands={{
+            'linktree': () => {return <ul><li><a href="https://www.youtube.com/channel/UCGxmNncs5ihjB-xk_9UUHyw" target="_blank">Youtube</a></li><li><a href="https://github.com/Gomez0015" target="_blank">Github</a></li></ul>},
+            showmsg: showMsg,
+            popup: () => alert('Terminal in React')
+          }}
+          descriptions={{
+            'linktree': 'all my links (Github, Youtube, ect)',
+            showmsg: 'shows a message',
+            alert: 'alert', popup: 'alert'
+          }}
+          msg='
+_______                                      ______             ________                     
+__  __ \__________________ ________   ______ ___  /_______ _    ___  __ \_____ ____  _______ 
+_  / / /_  ___/  ___/  __ `/_  ___/   _  __ `/_  //_/  __ `/    __  /_/ /  __ `/_  |/_/  __ \
+/ /_/ /_(__  )/ /__ / /_/ /_  /       / /_/ /_  ,<  / /_/ /     _  _, _// /_/ /__>  < / /_/ /
+\____/ /____/ \___/ \__,_/ /_/        \__,_/ /_/|_| \__,_/      /_/ |_| \__,_/ /_/|_| \____/ 
+‎                                                 
+Try typing help'
+          
+        />
+      </div>
     </div>
   );
 }
